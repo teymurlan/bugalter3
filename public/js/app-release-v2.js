e43c3ba4a7652bd6287e1f2c8f84034709ff4ea9
teymurlan <teymurlan@bk.ru>
2026-09-16 09:58:06 +0300

    Bust nested client bundle cache for release 40

--- public/js/app-release-v2.js
+++ public/js/app-release-v2.js
@@ -1,3 +1,3 @@
-import './api-release-v2.js?v=37';
-import './state-release-v2.js?v=35';
-import './app-release.js?v=37';
+import './api-release-v2.js?v=40';
+import './state-release-v2.js?v=40';
+import './app-release.js?v=40';
