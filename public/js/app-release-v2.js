5dec2347dcadc63902aa894a218fa99daa2e0c6d
teymurlan <teymurlan@bk.ru>
2026-09-16 21:59:02 +0300

    Advance client cache chain to release 54

--- public/js/app-release-v2.js
+++ public/js/app-release-v2.js
@@ -1,4 +1,4 @@
-import './api-release-v2.js?v=53';
-import './api-shared-v50.js?v=53';
-import './state-release-v2.js?v=53';
-import './app-release.js?v=53';
+import './api-release-v2.js?v=54';
+import './api-shared-v50.js?v=54';
+import './state-release-v2.js?v=54';
+import './app-release.js?v=54';
