797609a8e071829785bcb1283b61373cd66f9362
teymurlan <teymurlan@bk.ru>
2026-09-16 13:55:29 +0300

    Load shared availability API v50

--- public/js/app-release-v2.js
+++ public/js/app-release-v2.js
@@ -1,3 +1,4 @@
-import './api-release-v2.js?v=44';
-import './state-release-v2.js?v=44';
-import './app-release.js?v=49';
+import './api-release-v2.js?v=50';
+import './api-shared-v50.js?v=50';
+import './state-release-v2.js?v=50';
+import './app-release.js?v=50';
