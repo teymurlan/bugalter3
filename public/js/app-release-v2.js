90d3e3e9d7d94210f8163908f579c97ad1cf4c7a
teymurlan <teymurlan@bk.ru>
2026-09-18 10:48:15 +0300

    Refresh Ultra 7 client bundle

--- public/js/app-release-v2.js
+++ public/js/app-release-v2.js
@@ -1,4 +1,4 @@
-import './api-release-v2.js?v=54';
-import './api-shared-v50.js?v=54';
-import './state-release-v2.js?v=54';
-import './app-release.js?v=54';
+import './api-release-v2.js?v=57';
+import './api-shared-v50.js?v=57';
+import './state-release-v2.js?v=57';
+import './app-release.js?v=57';
