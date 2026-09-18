a59b2373c1157218f6ddc05658ad84d53f5ea3e6
teymurlan <teymurlan@bk.ru>
2026-09-18 18:06:41 +0300

    Client v60: load stable booking schedule

--- public/js/views/booking-v5.js
+++ public/js/views/booking-v5.js
@@ -1,4 +1,4 @@
-import { renderBooking as renderBaseBooking } from './booking-v2.js?v=52';
+import { renderBooking as renderBaseBooking } from './booking-v2.js?v=60';
 import { state } from '../state.js';
 import { showToast } from '../utils.js';
 
