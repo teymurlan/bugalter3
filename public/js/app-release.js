696d80c6975ab5a56068bdeb0289c67315ab13dd
teymurlan <teymurlan@bk.ru>
2026-09-18 21:35:43 +0300

    Bump client view imports to release 62

--- public/js/app-release.js
+++ public/js/app-release.js
@@ -1,10 +1,10 @@
 import { api, isDemoMode } from './api.js';
 import { state } from './state.js';
 import { escapeHtml } from './utils.js';
-import { renderBooking } from './views/booking-v5.js?v=61';
-import { renderConciergeHome } from './views/concierge-home-v4.js?v=61';
-import { renderConciergeOrders } from './views/concierge-orders-v4.js?v=61';
-import { renderConciergeProfile } from './views/concierge-profile-v4.js?v=61';
+import { renderBooking } from './views/booking-v5.js?v=62';
+import { renderConciergeHome } from './views/concierge-home-v4.js?v=62';
+import { renderConciergeOrders } from './views/concierge-orders-v4.js?v=62';
+import { renderConciergeProfile } from './views/concierge-profile-v4.js?v=62';
 import { renderAdmin } from './views/admin-v5.js?v=44';
 
 const tg = window.Telegram?.WebApp;
