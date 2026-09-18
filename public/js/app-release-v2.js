6d5102c4dc6af16e90797cb559e259317b04ba11
teymurlan <teymurlan@bk.ru>
2026-09-18 16:59:40 +0300

    Refresh Ultra 7.2 bundle cache

--- public/js/app-release-v2.js
+++ public/js/app-release-v2.js
@@ -1,4 +1,4 @@
-import './api-release-v2.js?v=58';
-import './api-shared-v50.js?v=58';
-import './state-release-v2.js?v=58';
-import './app-release.js?v=58';
+import './api-release-v2.js?v=59';
+import './api-shared-v50.js?v=59';
+import './state-release-v2.js?v=59';
+import './app-release.js?v=59';
