c25c551c893213fe268e780f90a961def46bcc0e
teymurlan <teymurlan@bk.ru>
2026-09-19 23:41:47 +0300

    Release 63: unified home action, fixed back and top feedback
    * Unify home booking and draft actions
    
    * Fix top back controls and feedback placement
    
    * Bump client shell to release 63
    
    * Bump client loader to release 63
    
    * Bump client module chain to release 63
    
    * Bump client views to release 63
    
    * Bump booking base cache to release 63
    
    * Open Telegram Mini App on release 63
    
    * Use release 63 in active Telegram menu wrapper
    
    * Update client cache assertions to release 63
    
    * Cover release 63 home and fixed feedback
    
    * Update release contract to 63

--- public/js/views/booking-v5.js
+++ public/js/views/booking-v5.js
@@ -1,4 +1,4 @@
-import { renderBooking as renderBaseBooking } from './booking-v2.js?v=62';
+import { renderBooking as renderBaseBooking } from './booking-v2.js?v=63';
 import { state } from '../state.js';
 import { showToast } from '../utils.js';
 
