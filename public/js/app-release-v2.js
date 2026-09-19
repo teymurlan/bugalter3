cab96949ad8cfa757da615753c75e11676783e8a
teymurlan <teymurlan@bk.ru>
2026-09-20 00:39:58 +0300

    Bump client module chain to release 65

--- public/js/app-release-v2.js
+++ public/js/app-release-v2.js
@@ -1,4 +1,4 @@
-import './api-release-v2.js?v=64';
-import './api-shared-v50.js?v=64';
-import './state-release-v2.js?v=64';
-import './app-release.js?v=64';
+import './api-release-v2.js?v=65';
+import './api-shared-v50.js?v=65';
+import './state-release-v2.js?v=65';
+import './app-release.js?v=65';
